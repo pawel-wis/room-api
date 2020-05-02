60a8ca73f875e8fbb02e02844e4c7a603380be1a
pawel.wis <pawel.wis.me@gmail.com>
2020-05-02 15:06:33 +0200

    Fix db url

--- config/DatabaseConnector.php
+++ config/DatabaseConnector.php
@@ -1,6 +1,6 @@
 <?php
 
-use \EnvLoader;
+require_once("EnvLoader.php");
 
 class DatabaseConnector {
     // DB configuration, conn is connection object it will be returned by connect method
@@ -18,16 +18,15 @@ class DatabaseConnector {
         $this->db_host = $env['DB_HOST'];
         $this->db_name = $env['DB_NAME'];
         $this->db_username = $env['DB_USERNAME'];
-        $this->db_password = $env['DB_PASSSWORD'];
+        $this->db_password = $env['DB_PASSWORD'];
     }
 
     //Init db connection
     public function connect(){
         $this->conn = null;
-
         try{
-            $this->conn = new PDO('mysql:host=' . $this->db_host . ';dbname=' . $this->db_name,
-                                  $this->db_username, $this->db_password);
+            $connection_url = 'mysql:host=' . $this->db_host . ';dbname=' . $this->db_name;
+            $this->conn = new PDO($connection_url, $this->db_username, $this->db_password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e){
             echo('Database connection error: ' . $e->getMessage());
