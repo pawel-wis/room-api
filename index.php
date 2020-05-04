240dba2ebe405ffaf66c997ce7e960297aed76ec
pawel.wis <pawel.wis.me@gmail.com>
2020-05-04 16:03:51 +0200

    create routing

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 require_once('config/DatabaseConnector.php');
+require_once('app/Controller/WConditionsController.php');
 
-// Parse url and pass data to controller 
+// splitted request url
+$splitted_url = explode('/', $_SERVER['REQUEST_URI']);
+ var_dump($splitted_url);
+
+ switch($splitted_url[2]){
+     case '':
+     break;
+
+     case 'weather':
+        $method = $_SERVER['REQUEST_METHOD'];
+        $controller = new WConditionsController;
+        $controller->hello();
+     break;
+
+     default:
+        echo("WRONG request");
+     break;
+ }
